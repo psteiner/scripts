1e44311adc456b14e23d97275ce8fa86709cecdc
Philip Steiner <philip@psteiner.com>
2013-03-17 11:31:37 -0700

    add multibyte support for python/Ultisnips compatibility

--- build_my_vim.sh
+++ build_my_vim.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /tmp/vim
-./configure --enable-pythoninterp=yes --enable-rubyinterp=yes --enable-gui=no
+cd /tmp/vim73
+./configure --enable-pythoninterp=yes --enable-rubyinterp=yes --enable-gui=no --with-features=big
 make && make install
 cd
